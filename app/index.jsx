d7c19d28e68173655b385537ea6a1d1cbae4e1c2
Sagnik Nandy <sagniknandy27@gmail.com>
2026-07-13 11:45:28 +0530

    commit

--- app/index.jsx
+++ app/index.jsx
@@ -52,7 +52,7 @@ const InitialScreen = () => {
 
         <View style={styles.textContainer}>
           <Text style={[darkTheme.typography.headerTitle, styles.headline]}>
-            {baseContent.header} Dev
+            {baseContent.header}
           </Text>
           <Text style={[darkTheme.typography.bodyMuted, styles.subHeadline]}>
             {baseContent.subHeader}
